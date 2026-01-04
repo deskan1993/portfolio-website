fc7dc216c291dc1d890edc7c45e3763ebb22b131
Andreas Nilsson <22604392+deskan1993@users.noreply.github.com>
2026-01-04 22:20:50 +0100

    Added DOM element references for contact form

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+//DOM elements
+const form = document.getElementById("contactForm");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const resetBtn = document.getElementById("resetBtn");
+
